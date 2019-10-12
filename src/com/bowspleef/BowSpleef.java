d383f984973275bd665abd849216e429e421b8a0
Elliott Olson <elolson@iastate.edu>
2019-10-12 12:29:42 -0500

    Setting up basic Bukkit plugin

--- src/com/bowspleef/BowSpleef.java
+++ src/com/bowspleef/BowSpleef.java
@@ -1,7 +1,17 @@
 package com.bowspleef;
 
-public class BowSpleef {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class BowSpleef extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        getLogger().info("BowSpleef is enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("BowSpleef is disabled.");
+    }
 
 }
